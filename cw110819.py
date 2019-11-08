7c87185db8b71c509791a126db6c0b0c95455337
Michael Roth <michael.franklin.roth@gmail.com>
2019-11-08 17:02:59 -0500

    sum_cubes cw

--- cw110819.py
+++ cw110819.py
@@ -18,3 +18,29 @@ def bmi(weight, height):
 # Test.assert_equals(bmi(90, 1.80), "Overweight")
 # Test.assert_equals(bmi(110, 1.80), "Obese")
 # Test.assert_equals(bmi(50, 1.50), "Normal")
+
+# -=-=-=-=-=-=-=-
+
+# https://www.codewars.com/kata/59a8570b570190d313000037/train/python
+
+
+def sum_cubes(n):
+    sum = 0
+    for x in range(n):
+        sum += (x+1)**3
+        print(sum)
+    return sum
+
+# better practice:
+# def sum_cubes(n):
+#     return sum(i**3 for i in range(0, n+1))
+
+
+sum_cubes(1)
+
+# test.assert_equals(sum_cubes(1), 1)
+# test.assert_equals(sum_cubes(2), 9)
+# test.assert_equals(sum_cubes(3), 36)
+# test.assert_equals(sum_cubes(4), 100)
+# test.assert_equals(sum_cubes(10), 3025)
+# test.assert_equals(sum_cubes(123), 58155876)
